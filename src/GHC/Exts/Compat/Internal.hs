36ca1b1db1ecb5c4397b1cd6eba6607e7b9fce98
brian <littlebubu.shu@gmail.com>
2022-09-09 16:17:42 -0400

    fix ghc 9.0.2

--- src/GHC/Exts/Compat/Internal.hs
+++ src/GHC/Exts/Compat/Internal.hs
@@ -6,7 +6,7 @@ module GHC.Exts.Compat.Internal where
 import GHC.Exts
 import Unsafe.Coerce
 
-#if !MIN_VERSION_base(4,15,0)
+#if !MIN_VERSION_base(4,16,0)
 mutableByteArrayContents# :: MutableByteArray# s -> Addr#
 mutableByteArrayContents# marr# = byteArrayContents# (unsafeCoerce# marr#)
 {-# INLINE mutableByteArrayContents# #-}
