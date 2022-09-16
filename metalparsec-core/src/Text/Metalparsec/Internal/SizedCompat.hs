c4fdbbb24b633b10ad7a027df0dac5a2d7a3f2d1
brian <littlebubu.shu@gmail.com>
2022-09-16 19:32:17 -0400

    add ghc < 8.8 support

--- metalparsec-core/src/Text/Metalparsec/Internal/SizedCompat.hs
+++ metalparsec-core/src/Text/Metalparsec/Internal/SizedCompat.hs
@@ -47,7 +47,9 @@ indexWord8Array# = E.indexWord8Array#
 readWord8Array# = E.readWord8Array#
 pattern W8# w# = W.W8# w#
 {-# INLINE W8# #-}
-#else
+#endif
+
+#if MIN_VERSION_BASE(4,13,0)
 -- GHC <9.2
 type Word8# = E.Word8#
 
@@ -60,18 +62,21 @@ readWord8Array# bs# i# s# = case E.readWord8Array# bs# i# s# of
 pattern W8# w# <- W.W8# (wordToWord8# -> w#)
   where
     W8# w# = W.W8# (word8ToWord# w#)
-#endif
-
-#if !MIN_VERSION_base(4,13,0)
+#else
 -- GHC <8.8
 
 type Word8# = Word#
+
 narrowWord8# :: Word# -> Word8#
 narrowWord8# = narrow8Word#
 extendWord8# :: Word# -> Word8#
 extendWord8# w# = w#
-leWord8# :: Word8# -> Word8# -> Int#
-leWord8# w1# w2# = leWord# w1# w2#
-eqWord8# :: Word8# -> Word8# -> Int#
+
 eqWord8# w1# w2# = eqWord# w1# w2#
+indexWord8Array# bs# i# = wordToWord8# (E.indexWord8Array# bs# i#)
+readWord8Array# bs# i# s# = case E.readWord8Array# bs# i# s# of
+  (# s#, w# #) -> (# s#, wordToWord8# w# #)
+pattern W8# w# <- W.W8# (wordToWord8# -> w#)
+  where
+    W8# w# = W.W8# (word8ToWord# w#)
 #endif
