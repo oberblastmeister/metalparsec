bcdaf61178996df8628f395621e50cc8d0fd3b8d
Brian Shu <littlebubu.shu@gmail.com>
2022-09-23 18:32:20 -0400

    update benchmarks and tests

--- metalparsec/bench/Expr.hs
+++ metalparsec/bench/Expr.hs
@@ -2,19 +2,30 @@ module Expr where
 
 import Control.DeepSeq (NFData)
 import qualified Expr.Attoparsec as Attoparsec
+#ifdef FLATPARSE
 import qualified Expr.Flatparse as Flatparse
+#endif
 import qualified Expr.Metalparsec as Metalparsec
 import Gauge.Main
 import Util
+flatparseBenches :: [Benchmark]
+#ifdef FLATPARSE
+flatparseBenches = [
+  test bytestring "Flat" (flatParseStateful Flatparse.expr)
+                   ]
+#else
+flatparseBenches = []
+#endif
+
 
 main :: Benchmark
 main =
   bgroup
     "expr"
-    [ test bytestring "Flat" (flatParseStateful Flatparse.expr),
+    $ [ 
       test text "Metal" (metalParse Metalparsec.expr),
       test bytestring "Atto" (attoParseByteString Attoparsec.expr)
-    ]
+    ] ++ flatparseBenches
   where
     test :: (NFData rep) => (FilePath -> IO rep) -> String -> (rep -> res) -> Benchmark
     test = makeBenchmark' ["bench/inputs/big-example.txt"]
--- metalparsec/bench/Simple.hs
+++ metalparsec/bench/Simple.hs
@@ -7,8 +7,10 @@ import Data.String (fromString)
 import qualified Data.Text as T
 import Gauge
 import qualified Simple.Attoparsec as Attoparsec
+#ifdef FLATPARSE
 import qualified Simple.FPBasic as FPBasic
 import qualified Simple.FPStateful as FPStateful
+#endif
 import qualified Simple.Megaparsec as Megaparsec
 import qualified Simple.Metalparsec as Metalparsec
 import qualified Simple.MetalparsecTH as MetalparsecTH
@@ -26,38 +28,73 @@ numcsvInp = concat ("0" : [(",  " ++ show n) | n <- [1 .. 100000 :: Int]])
 readIntInp :: B.ByteString
 readIntInp = "12345678910"
 
+  
 main :: Benchmark
 main =
   bgroup
     "simple"
-    [ env (pure (fromString sexpInp :: B.ByteString, fromString sexpInp :: T.Text)) $ \(~(sexpInp, sexpInpText)) ->
-        bgroup
-          "sexp"
-          [ bench "metalparsec" $ whnf Metalparsec.runSexp sexpInpText,
-            bench "metalparsecth" $ whnf MetalparsecTH.runSexp sexpInpText,
-            bench "fpbasic" $ whnf FPBasic.runSexp sexpInp,
-            bench "fpstateful" $ whnf FPStateful.runSexp sexpInp,
-            bench "attoparsec" $ whnf Attoparsec.runSexp sexpInp,
-            bench "megaparsec" $ whnf Megaparsec.runSexp sexpInp
-          ],
-      env (pure (fromString longwsInp :: B.ByteString, fromString longwsInp :: T.Text)) $ \(~(longwsInp, longwsInpText)) ->
-        bgroup
-          "long keyword"
-          [ bench "metalparsec" $ whnf Metalparsec.runLongws longwsInpText,
-            bench "metalparsecth" $ whnf MetalparsecTH.runLongws longwsInpText,
-            bench "fpbasic" $ whnf FPBasic.runLongws longwsInp,
-            bench "fpstateful" $ whnf FPStateful.runLongws longwsInp,
-            bench "attoparsec" $ whnf Attoparsec.runLongws longwsInp,
-            bench "megaparsec" $ whnf Megaparsec.runLongws longwsInp
-          ],
+    [ sexp,
+      long,
+      csv
+    ]
+  where
+    csv =
       env (pure (fromString numcsvInp :: B.ByteString, fromString numcsvInp :: T.Text)) $ \(~(numcsvInp, numcsvInpText)) ->
         bgroup
           "numeral csv"
-          [ bench "metalparsec" $ whnf Metalparsec.runNumcsv numcsvInpText,
+          $ [ bench "metalparsec" $ whnf Metalparsec.runNumcsv numcsvInpText,
             bench "metalparsecth" $ whnf MetalparsecTH.runNumcsv numcsvInpText,
-            bench "fpbasic" $ whnf FPBasic.runNumcsv numcsvInp,
-            bench "fpstateful" $ whnf FPStateful.runNumcsv numcsvInp,
             bench "attoparsec" $ whnf Attoparsec.runNumcsv numcsvInp,
             bench "megaparsec" $ whnf Megaparsec.runNumcsv numcsvInp
-          ]
-    ]
+          ] ++ flatparse
+      where
+        flatparse :: [Benchmark]
+#ifdef FLATPARSE
+        flatparse = [
+            bench "fpbasic" $ whnf FPBasic.runNumcsv numcsvInp,
+            bench "fpstateful" $ whnf FPStateful.runNumcsv numcsvInp
+                    ]
+#else
+        flatparse = []
+#endif
+
+    long =
+      env (pure (fromString longwsInp :: B.ByteString, fromString longwsInp :: T.Text)) $ \(~(longwsInp, longwsInpText)) ->
+        bgroup
+          "long keyword"
+          $ [ bench "metalparsec" $ whnf Metalparsec.runLongws longwsInpText,
+            bench "metalparsecth" $ whnf MetalparsecTH.runLongws longwsInpText,
+            bench "attoparsec" $ whnf Attoparsec.runLongws longwsInp,
+            bench "megaparsec" $ whnf Megaparsec.runLongws longwsInp
+          ] ++ flatparse
+      where
+        flatparse :: [Benchmark]
+#ifdef FLATPARSE
+        flatparse = [
+                bench "fpbasic" $ whnf FPBasic.runLongws longwsInp,
+                bench "fpstateful" $ whnf FPStateful.runLongws longwsInp,
+                           ]
+#else
+        flatparse = []
+#endif
+
+    sexp =
+      env (pure (fromString sexpInp :: B.ByteString, fromString sexpInp :: T.Text)) $ \(~(sexpInp, sexpInpText)) ->
+          bgroup
+            "sexp"
+            $ [ bench "metalparsec" $ whnf Metalparsec.runSexp sexpInpText,
+              bench "metalparsecth" $ whnf MetalparsecTH.runSexp sexpInpText,
+              bench "attoparsec" $ whnf Attoparsec.runSexp sexpInp,
+              bench "megaparsec" $ whnf Megaparsec.runSexp sexpInp
+            ] ++ flatparse
+      where
+        flatparse :: [Benchmark]
+#ifdef FLATPARSE
+        flatparse = [
+                          bench "fpbasic" $ whnf FPBasic.runSexp sexpInp,
+                          bench "fpstateful" $ whnf FPStateful.runSexp sexpInp
+                           ]
+#else
+        flatparse = []
+#endif
+
